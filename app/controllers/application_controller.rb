df7c0b377b1e19c1eb6f9725f337da2a74e61b20
Manu <manfergo25@gmail.com>
2014-10-22 16:40:00 -0430

    Init Commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  def after_sign_in_path_for(resource)
+    users_path() #your path
+  end
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
